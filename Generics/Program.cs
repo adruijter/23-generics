2ceb9b8012ec0ad68160e7fd76b077ab62455a4b
Arjan de Ruijter <rra@mboutrecht.nl>
2015-01-06 10:34:57 +0100

    Initial commit generics

---
+++ Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Generics!";
+
+            // We maken een object van de generieke klas List
+            List<int> listMetGeheleGetallen;
+            listMetGeheleGetallen = new List<int>();
+
+            listMetGeheleGetallen.Add(100);
+            listMetGeheleGetallen.Add(200);
+            listMetGeheleGetallen.Add(300);
+            listMetGeheleGetallen.Add(400);
+            listMetGeheleGetallen.Add(500);
+
+            string getallen = "";
+            foreach (int getal in listMetGeheleGetallen)
+            {
+                getallen += getal.ToString() + " ";
+            }
+
+            Console.WriteLine("In de List<int> listMetGeheleGetallen zit het getal {0}", getallen);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
